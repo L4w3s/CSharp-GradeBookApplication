d2cb9f8f31267eda58b303d55ebd35b850573427
Jordan Lawes <l4w3s@live.co.uk>
2019-07-22 21:50:25 +0100

    Added IsWeighted boolean to BaseGradeBook
    This new boolean will modify the output of the GetGPA method within BaseGradeBook, adding 1 to the output where certain conditions are met, including the boolean being true.
    The CreateCommand and HelpCommand within StartingUserInterface has also been updated to include this boolean.

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,10 +14,12 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool weighted)
         {
             Name = name;
+            IsWeighted = weighted;
             Students = new List<Student>();
         }
 
@@ -107,20 +109,28 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var modifier = IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled) ? 1 : 0;
+            var grade = 0;
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    grade = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    grade = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    grade = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    grade = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    grade = 0;
+                    break;
             }
-            return 0;
+            return grade + modifier;
         }
 
         public virtual void CalculateStatistics()
