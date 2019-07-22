30df41d7d0bfe92290cef46b2b281f672bd854c3
Jordan Lawes <l4w3s@live.co.uk>
2019-07-22 21:40:55 +0100

    BaseGradeBook is now abstract
    I have set the BaseGradeBook class to be abstract, I have also updated the CreateCommand method within StartingUserInterface as it was creating a new instance of BaseGradeBook prior to checking the type provided by the user, this is not allowed due to the class now being abstract.

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -42,7 +42,7 @@ namespace GradeBook.UserInterfaces
             var name = parts[1];
             var type = parts[2];
 
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            BaseGradeBook gradeBook;
             if (type == "standard") gradeBook = new StandardGradeBook(name);
             else if (type == "ranked") gradeBook = new RankedGradeBook(name);
             else
